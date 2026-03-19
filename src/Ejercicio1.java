6f0b53bea24ba3eecefde9624050f1cb96186b66
Moises <mocaor2007@gmail.com>
2026-03-19 13:22:24 +0100

    a

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio1 {
     static void main(String[] args) {
+        File f = new File("./src/leeme.txt");
 
+
+
+        try (FileReader fr = new FileReader(f);
+             BufferedReader br = new BufferedReader(fr)){
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
